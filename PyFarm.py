d22ce7068d3947abdb0d36d5801a2c3d16eefa44
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-01-04 22:49:58 -0500

    Update PyFarm.py

---
+++ PyFarm.py
@@ -0,0 +1,24 @@
+import pymysql
+
+arg = []
+outputs = []
+
+def runScript(path, args):
+    # make connection
+    # push script
+    # push args
+    # wait for response
+    print("run")
+
+
+def input(num):
+    print("input")
+
+
+def setInput(listArgs):
+    arg = listArgs
+
+def output(val):
+    outputs.append(val)
+    
+
