45b877f23063a14a5ae9d0475169c7cfafea18e7
Jared Maki <jemaki56@gmail.com>
2019-01-05 17:12:15 -0500

    Refactors

--- PyFarm.py
+++ PyFarm.py
@@ -1,4 +1,6 @@
 import gspread
+import time
+import PyFarmChild
 from oauth2client.service_account import ServiceAccountCredentials
 
 arg = []
@@ -6,46 +8,17 @@ outputs = []
 idnum = 0
 sheet = None
 client = None
+rowCount = None
 
 def runScript(path, args):
-    
-    # read file and store into a string
-    py_file = open(path)
-    file_content =  py_file.read()
-    py_file.close()
-
+    # Called by user; starts execution of the script 
+    file_content = readFile(path)
     makeConnection()
+    pushScript(file_content)
+    pushArgs(args)
+    #processOutput()        
 
-    global sheet
-    sheet = client.open('pyfarm-hh').get_worksheet(1)
     
-    # push script
-    sheet.update_cell(1, 1, file_content)
-
-    # push args
-    sheet = client.open('pyfarm-hh').get_worksheet(0)
-    progress = 0
-    output = None
-   # for argument in args:
-   #     sheet.update_cell(sheet.row_count, 1, "0")
-   #     for arg_index in range(argument.__len__()):
-   #         sheet.update_cell(sheet.row_count, argument.col + 1, argument[arg_index])
-
-    i = 1
-    for argument in args:
-        j = 2
-        for val in argument:
-            sheet.update_cell(i, 1, "-1")
-            sheet.update_cell(i, j, val)
-            j+=1
-        sheet.update_cell(i, j, "N/A")    
-        i+=1
-        
-            
-
-    # wait for response
-
-    print("Waiting for output...")
 
 def makeConnection():
     # Make the connection to the Google Spreadsheet
@@ -65,5 +38,57 @@ def setInput(listArgs):
 
 def output(val):
     outputs.append(val)
+
+def readFile(path):
+    # read file and store into a string
+    py_file = open(path)
+    scanned_text =  py_file.read()
+    py_file.close()
+    return scanned_text
+
+def getWorkSheetOne():
+    # Get the worksheet containing progress, args, and output
+    global sheet
+    sheet = client.open('pyfarm-hh').get_worksheet(0)
+    return sheet
+
+def getWorkSheetTwo():
+    # Get the worksheet containing script, and machine ids
+    global sheet
+    sheet = client.open('pyfarm-hh').get_worksheet(1)
+    return sheet
+
+def pushScript(file_content):
+    global sheet
+    sheet = getWorkSheetTwo()
+    sheet.update_cell(1, 1, file_content)
+
+def pushArgs(args):
+    global sheet
+    sheet = getWorkSheetOne()
+    i = 1
+    for argument in args:
+        j = 2
+        for val in argument:
+            sheet.update_cell(i, 1, "0")
+            sheet.update_cell(i, j, val)
+            j+=1
+        sheet.update_cell(i, j, "N/A")    
+        i+=1
+    global rowCount    
+    rowCount = i
+
+def processOutput():
+    # wait for response
+    print("Waiting for output...")
+
+    while outputs.__len__() != rowCount:
+        time.sleep(5)
+
+    print("Processing output...")
+    for out in outputs:
+        print(out)    
+
+
 
 
