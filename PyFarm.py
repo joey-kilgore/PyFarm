cb0c9496387d204e8de692183fb4c3722c046132
Jared Maki <jemaki56@gmail.com>
2019-01-05 05:38:16 -0500

    Functionality to push script to server

--- PyFarm.py
+++ PyFarm.py
@@ -1,14 +1,26 @@
-import pymysql
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 arg = []
 outputs = []
 
 def runScript(path, args):
     # make connection
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('pyfarm-hh').sheet1
+    new_row = [path, 'foo']
+    sheet.append_row(new_row)
+    #sheet.update_cell(4, 1, path)
+
     # push script
+    server_storage = path
     # push args
     # wait for response
     print("run")
+    print(server_storage)
 
 
 def input(num):
